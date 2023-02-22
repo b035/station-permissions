e49e55579bcbf20de6ea16696879396ffd4d5fce
b035 <marlon@erler.cc>
2023-02-22 19:54:56 +0100

    renamed create() -> create_simple()

--- permissions.ts
+++ permissions.ts
@@ -5,7 +5,7 @@ import * as SDK from "@the-stations-project/sdk";
 /* MAIN */
 async function main(subcommand: string, args: string[]) {
 	switch (subcommand) {
-		case "create": return await create(args[0]);
+		case "create_simple": return await create_simple(args[0]);
 		case "remove": return await remove(args[0]);
 		case "check": return await check(args[0], args[1]);
 		default: return new SDK.Result(SDK.ExitCodes.ErrNoCommand, undefined);
@@ -13,17 +13,17 @@ async function main(subcommand: string, args: string[]) {
 }
 
 /* SUB-FUNCTIONS */
-async function create(action: string) {
+async function create_simple(action: string) {
 	const result = new SDK.Result(SDK.ExitCodes.Ok, undefined);
 
 	/* safety */
 	if (SDK.contains_undefined_arguments(arguments)) return result.finalize_with_code(SDK.ExitCodes.ErrMissingParameter);
 
 	/* get path */
-	const path = SDK.Registry.join_paths("permissions", action);
+	const dir_path = SDK.Registry.join_paths("permissions", action);
 
-	/* create */
-	(await SDK.Registry.mkdir(path)).or_log_error()
+	/* create dir */
+	(await SDK.Registry.mkdir(dir_path)).or_log_error()
 		.err(() => result.finalize_with_code(SDK.ExitCodes.ErrUnknown));
 
 	return result;
