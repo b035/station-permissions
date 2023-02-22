f68efd902f82b7ad2b3c64e8b0f7381e3728b352
b035 <marlon@erler.cc>
2023-02-22 19:30:26 +0100

    created init script

---
+++ init.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import * as SDK from "@the-stations-project/sdk";
+
+async function init() {
+	const result = new SDK.Result(SDK.ExitCodes.Ok, undefined);
+
+	/* create directory */
+	(await SDK.Registry.mkdir("permissions"))
+		.err(() => result.finalize_with_code(SDK.ExitCodes.ErrUnknown));
+
+	return result;
+}
+
+init().then((result) => console.log(result.to_string()));
