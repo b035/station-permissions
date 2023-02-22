c458f0cc3330049cceb94b0688bf97693dbb0eb2
b035 <marlon@erler.cc>
2023-02-22 19:46:41 +0100

    added create() method

---
+++ permissions.js
@@ -0,0 +1,61 @@
+#! /usr/bin/env node
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const SDK = __importStar(require("@the-stations-project/sdk"));
+/* MAIN */
+async function main(subcommand, args) {
+    switch (subcommand) {
+        case "create": return await create(args[0]);
+        case "check": return await check(args[0], args[1]);
+        default: return new SDK.Result(SDK.ExitCodes.ErrNoCommand, undefined);
+    }
+}
+/* SUB-FUNCTIONS */
+async function create(action) {
+    const result = new SDK.Result(SDK.ExitCodes.Ok, undefined);
+    /* safety */
+    if (SDK.contains_undefined_arguments(arguments))
+        return result.finalize_with_code(SDK.ExitCodes.ErrMissingParameter);
+    /* get path */
+    const path = SDK.Registry.join_paths("permissions", action);
+    /* create */
+    (await SDK.Registry.mkdir(path)).or_log_error()
+        .err(() => result.finalize_with_code(SDK.ExitCodes.ErrUnknown));
+    return result;
+}
+async function check(action, uname) {
+    const result = new SDK.Result(SDK.ExitCodes.Ok, false);
+    /* safety */
+    if (SDK.contains_undefined_arguments(arguments))
+        return result.finalize_with_code(SDK.ExitCodes.ErrMissingParameter);
+    /* get path */
+    const path = SDK.Registry.join_paths("permissions", action, uname);
+    /* check permission */
+    (await SDK.Registry.test(path))
+        .ok(() => result.finalize_with_value(true));
+    return result;
+}
+SDK.start_module(main, (result) => console.log(result.to_string()));
---
+++ permissions.ts
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+import * as SDK from "@the-stations-project/sdk";
+
+/* MAIN */
+async function main(subcommand: string, args: string[]) {
+	switch (subcommand) {
+		case "create": return await create(args[0]);
+		case "check": return await check(args[0], args[1]);
+		default: return new SDK.Result(SDK.ExitCodes.ErrNoCommand, undefined);
+	}
+}
+
+/* SUB-FUNCTIONS */
+async function create(action: string) {
+	const result = new SDK.Result(SDK.ExitCodes.Ok, undefined);
+
+	/* safety */
+	if (SDK.contains_undefined_arguments(arguments)) return result.finalize_with_code(SDK.ExitCodes.ErrMissingParameter);
+
+	/* get path */
+	const path = SDK.Registry.join_paths("permissions", action);
+
+	/* create */
+	(await SDK.Registry.mkdir(path)).or_log_error()
+		.err(() => result.finalize_with_code(SDK.ExitCodes.ErrUnknown));
+
+	return result;
+}
+
+async function check(action: string, uname: string) {
+	const result = new SDK.Result(SDK.ExitCodes.Ok, false);
+
+	/* safety */
+	if (SDK.contains_undefined_arguments(arguments)) return result.finalize_with_code(SDK.ExitCodes.ErrMissingParameter);
+
+	/* get path */
+	const path = SDK.Registry.join_paths("permissions", action, uname);
+
+	/* check permission */
+	(await SDK.Registry.test(path))
+		.ok(() => result.finalize_with_value(true));
+
+	return result;
+}
+
+SDK.start_module(main, (result) => console.log(result.to_string()));
